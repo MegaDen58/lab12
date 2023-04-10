b4883755e798bd1c7e11b631d76a5808a2f2f16c
MegaDen58 <extreme.den2004@gmail.com>
2023-04-10 08:35:24 +0300

    w

---
+++ Program.cs
@@ -0,0 +1,37 @@
+using System;
+
+
+public delegate void MyDelegate(string text);
+class ClassWithEvent
+{
+    public event MyDelegate MyEvent;
+
+    public void GenerateEvent(string eventName)
+    {
+        MyEvent?.Invoke(eventName);
+    }
+}
+
+class SecondClass
+{
+    public void HandleEvent(string eventName)
+    {
+        Console.WriteLine($"Событие сгенерировано объектом: {eventName}");
+    }
+}
+class HelloWorld
+{
+    static void Main()
+    {
+        ClassWithEvent obj1 = new ClassWithEvent();
+        ClassWithEvent obj2 = new ClassWithEvent();
+        SecondClass obj3 = new SecondClass();
+
+        obj1.MyEvent += obj3.HandleEvent;
+        obj2.MyEvent += obj3.HandleEvent;
+
+        obj1.GenerateEvent("Первый объект");
+        obj2.GenerateEvent("Второй объект");
+
+    }
+}
